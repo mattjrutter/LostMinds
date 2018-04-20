e86593ea9cdc71924c267fe0093492ca84f42767
Mitchell Bohn <mwbohn@mavs.coloradomesa.edu>
2018-04-20 10:04:00 -0600

    Math functions
    Located at bottom of soundComponent.h

--- LostMinds/soundComponent.h
+++ LostMinds/soundComponent.h
@@ -37,3 +37,22 @@ public:
 
 	}
 };
+
+/*
+Functions that takes 4 double values and return an angle in degrees or distance.
+*/
+#include <cmath>
+
+int getDistance(double x1, double y1, double x2, double y2) {
+    return static_cast<int>(sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2)));
+}
+
+int getAngle(double x1, double y1, double x2, double y2) {
+    static const double TWOPI = 6.2831853071795865;
+    static const double RAD2DEG = 57.2957795130823209;
+    // if (x1 = x2 and y1 = y2) throw an error 
+    double theta = atan2(x2 - x1, y1 - y2);
+    if (theta < 0.0)
+        theta += TWOPI;
+    return ststic_cast<int>(RAD2DEG * theta);
+}
